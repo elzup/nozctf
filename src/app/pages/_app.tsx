05f5ece3ba57fbf3014e677cf9204b1ea9834976
elzup <guild0105@gmail.com>
2020-03-30 23:03:41 +0900

    fix: types

---
+++ src/app/pages/_app.tsx
@@ -0,0 +1,10 @@
+import { AppProps } from 'next/app'
+import 'normalize.css'
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Component {...pageProps} />
+  </>
+)
+
+export default App
