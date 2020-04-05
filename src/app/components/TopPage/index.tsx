c73b8f9bf69d55fc87babb9eb131a7a9490b6f1f
elzup <guild0105@gmail.com>
2020-04-05 21:45:02 +0900

    add List with login

--- src/app/components/TopPage/index.tsx
+++ src/app/components/TopPage/index.tsx
@@ -2,26 +2,48 @@ import * as firebase from 'firebase'
 import Link from 'next/link'
 import { useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { useDocumentData } from 'react-firebase-hooks/firestore'
 import { questions } from '../../questions'
 import { getFirestore } from '../../service/firebase'
 import App from '../App'
+import { User } from '../../types'
 
 const fdb = getFirestore()
 
-function TopPage() {
-  const [user, loading, error] = useAuthState(firebase.auth())
-  const [text, setText] = useState<string>('')
+function ListWithLogin({ uid }: { uid: string }) {
+  const [doc, loading] = useDocumentData<User>(fdb.collection('user').doc(uid))
+
+  console.log(doc)
 
   return (
-    <App>
-      <ul>
-        {questions.map((q) => (
-          <li key={q.num}>
-            <Link href={`/q/${q.num}`}>{q.text}</Link>
-          </li>
-        ))}
-      </ul>
-    </App>
+    <ul>
+      {questions.map((q) => (
+        <li key={q.num}>
+          <Link href={`/q/${q.num}`}>{q.text}</Link>
+        </li>
+      ))}
+    </ul>
   )
 }
+
+function List() {
+  return (
+    <ul>
+      {questions.map((q) => (
+        <li key={q.num}>
+          <Link href={`/q/${q.num}`}>{q.text}</Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+function TopPage() {
+  const [user, loading] = useAuthState(firebase.auth())
+
+  return (
+    <App>{!loading && user ? <ListWithLogin uid={user.uid} /> : <List />}</App>
+  )
+}
+
 export default TopPage
