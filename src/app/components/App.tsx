5f9503ccd8d40ef57a61d1624366b55f11552bac
elzup <guild0105@gmail.com>
2020-04-07 22:24:53 +0900

    login context

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,10 +1,79 @@
+import { useAuthState } from 'react-firebase-hooks/auth'
+import {
+  useState,
+  useEffect,
+  Dispatch,
+  SetStateAction,
+  createContext,
+} from 'react'
+import Router from 'next/router'
+import { getAuth, getFirestore } from '../service/firebase'
+import { User } from '../types'
 import Header from './Header'
 
-const App = ({ children }: { children?: unknown }) => (
-  <main>
-    <Header />
-    {children}
-  </main>
-)
+const { auth } = getAuth()
+const fdb = getFirestore()
+
+type LoginInfo =
+  | {
+      status: 'none'
+    }
+  | {
+      status: 'auth'
+      uid: string
+    }
+  | {
+      status: 'comp'
+      uid: string
+      user: User
+    }
+
+export const LoginContext = createContext<
+  [LoginInfo, Dispatch<SetStateAction<LoginInfo>>]
+>([
+  { status: 'none' },
+  () => {
+    //
+  },
+])
+
+// function useLogin() {
+//   // TODO
+// }
+
+const App = ({ children }: { children?: unknown }) => {
+  const [login, setLogin] = useState<LoginInfo>({ status: 'none' })
+  const [fuser, loading] = useAuthState(auth)
+
+  useEffect(() => {
+    if (loading) return
+    fdb
+      .collection('user')
+      .doc(fuser.uid)
+      .get()
+      .then((user) => {
+        if (user.exists) {
+          setLogin({
+            status: 'comp',
+            uid: fuser.uid,
+            user: user.data() as User,
+          })
+        } else {
+          if (Router.asPath !== '/register') {
+            Router.push('/register')
+          }
+        }
+      })
+  }, [loading, fuser && fuser.uid])
+
+  return (
+    <LoginContext.Provider value={[login, setLogin]}>
+      <main>
+        <Header />
+        {children}
+      </main>
+    </LoginContext.Provider>
+  )
+}
 
 export default App
--- src/app/components/TopPage/index.tsx
+++ src/app/components/TopPage/index.tsx
@@ -1,19 +1,11 @@
-import * as firebase from 'firebase'
 import Link from 'next/link'
-import { useState } from 'react'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { useDocumentData } from 'react-firebase-hooks/firestore'
+import { useContext } from 'react'
 import { questions } from '../../questions'
-import { getFirestore } from '../../service/firebase'
-import App from '../App'
 import { User } from '../../types'
+import App, { LoginContext } from '../App'
 
-const fdb = getFirestore()
-
-function ListWithLogin({ uid }: { uid: string }) {
-  const [doc, loading] = useDocumentData<User>(fdb.collection('user').doc(uid))
-
-  console.log(doc)
+function ListWithLogin({ user }: { user: User }) {
+  console.log(user)
 
   return (
     <ul>
@@ -39,11 +31,20 @@ function List() {
 }
 
 function TopPage() {
-  const [user, loading] = useAuthState(firebase.auth())
+  const [login] = useContext(LoginContext)
 
+  if (login.status === 'comp') {
+    return <ListWithLogin user={login.user} />
+  }
+  return <List />
+}
+
+function TopPageContainer() {
   return (
-    <App>{!loading && user ? <ListWithLogin uid={user.uid} /> : <List />}</App>
+    <App>
+      <TopPage />
+    </App>
   )
 }
 
-export default TopPage
+export default TopPageContainer
