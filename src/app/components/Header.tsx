7d6d378f82f8c54e8491d25c05a0baa0a94e92c7
elzup <guild0105@gmail.com>
2020-04-08 23:45:47 +0900

    view user id

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,17 @@
 import Link from 'next/link'
+import { useContext } from 'react'
+import { Typography } from '@material-ui/core'
 import LoginButton from './LoginButton'
+import { LoginContext } from './App'
 
-export default ({ pathname }: { pathname?: unknown }) => (
-  <header>
-    Score Form
-    <LoginButton />
-  </header>
-)
+export default ({ pathname }: { pathname?: unknown }) => {
+  const [login] = useContext(LoginContext)
+
+  return (
+    <header>
+      Score Form
+      {login.status === 'comp' && <Typography>{login.user.id}</Typography>}
+      <LoginButton />
+    </header>
+  )
+}
