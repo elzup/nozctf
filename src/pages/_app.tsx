9b934dfd4388bac0a5be2f1741d52d2e6c768ad4
elzup <guild0105@gmail.com>
2021-08-04 11:52:42 +0900

    omg

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,7 +20,6 @@ const App = ({ Component, pageProps }: AppProps) => (
 
       <meta charSet="utf-8" />
       <meta name="theme-color" content="#000051" />
-      <meta name="robots" content="noindex,nofollow,noarchive" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta property="og:site_name" content="nozctf" />
